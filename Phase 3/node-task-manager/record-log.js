d5d61e1819352b5bae27edb9672add9d79849038
Julian <julianprice8@gmail.com>
2021-08-30 23:29:14 -0400

    Removed unused file and renamed record-log2.js to record-log.js.

--- Phase 3/node-task-manager/record-log.js
+++ Phase 3/node-task-manager/record-log.js
@@ -1,117 +1,108 @@
 let http = require("http");
+let fs = require("fs");
 let url = require("url");
-let records = [
-    {"user":"Raj","taskID":"123"},
-    {"user":"Ramesh","taskID":"567"},
-    {"user":"Raju","taskID":"1100"},
-]
+let records = [];
 let indexPage = `
             <html>
-                    <head>
-
-                    </head>
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Node Task Manager</title>
+                </head>
                     <body>
-                    <h2>Welcome to Http Module</h2>
-                    <a href="Login">Task Manager</a>
-                    </body>
-            </html>
-`
-let nodeTaskManagerPage = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Node Task Manager</title>
-</head>
-<body>
-    <h2>Task Manager Page</h2>
-    <form action="checkTaskID">
-        <label>UserName</label>
-        <input type="text" name="user"/><br/>
-        <label>Password</label>
-        <input type="password" name="pass"/><br/>
-        <input type="submit" value="submit"/>
-       <input type="reset" value="reset"/> <br/>
-       <a href="signup">Sign Up</a>
-    </form>
-</body>
-</html> 
+                    <h2>Task Manager</h2>
+                        <p>Task ID must be unique. If task ID is the same, it will remove the task from the records.</p>
+                        <form action="task_manager_form">
+                        <label>Employee ID</label>
+                        <input type="number" name="employee_id"/><br/>
+                        <label>Task ID</label>
+                        <input type="number" name="task_id"/><br/>
+                        <label>Task</label>
+                        <input type="text" name="task"/><br/>
+                        <label>Deadline</label>
+                        <input type="text" name="deadline"/><br/>
+                
+                        <input type="submit" value="Add or Delete Task"/>
+                    <input type="reset" value="reset"/> 
+                </form>
 `
 
-let registerLoginPage=`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Node Task Manager</title>
-</head>
-<body>
-    <h2>Registration Page</h2>
-    <form action="register">
-        <label>Employee ID</label>
-        <input type="number" name="employee_id"/><br/>
-        <label>Task ID</label>
-        <input type="number" name="task_id"/><br/>
-        <label>Task</label>
-        <input type="text" name="task"/><br/>
-        <label>Deadline</label>
-        <input type="text" name="deadline"/><br/>
+let table = `<br>
+<table border="1">
+    <tr>
+        <th>Employee ID</th>
+        <th>Task ID</th>
+        <th>Task</th>
+        <th>Deadline</th>
+    </tr>`;
+let endOfIndexPage = `</table></body></html>`
 
-        <input type="submit" value="submit"/>
-       <input type="reset" value="reset"/> 
-    </form>
-</body>
-</html>
-`
-let server = http.createServer((request,response)=> {
+let server = http.createServer((request,response) => {
+    fs.writeFile("record.json", "", function() {
+    });
     let urlInfo = url.parse(request.url,true);
-    if(urlInfo.path != "/favicon.ico"){
-        if(urlInfo.path == "/Login"){
-            response.write(nodeTaskManagerPage);
-        } else if(urlInfo.pathname == "/checkTaskID"){
-                let login = urlInfo.query;
-                let input = urlInfo.query;
-                let result = records.find(l => l.taskID);
-                if(result != undefined){
-                        response.write("Successful Data Entry!" + result.taskID);
+    if (urlInfo.path != "/favicon.ico") {
+
+        let input = urlInfo.query;
+        
+        let result = records.find(r=>r.task_id == input.task_id);
+        if (result == undefined && input.task_id != undefined) { 
+            response.writeHead(200,{"content-type":"text/html"});
+            records.push(input);
+            fs.writeFileSync("record.json", JSON.stringify(records), (error) => {
+                if (error) {
+                    console.log(error);
                 } else {
-                        response.write("Failure try once again!");
+                    console.log("Data appended to file.")
                 }
-        } else if(urlInfo.path =="/signup"){
-                response.write(registerLoginPage);
-        } else if(urlInfo.pathname == "/register"){
-                let input = urlInfo.query;
-                let result = records.find(l => l.taskID == input.taskID);
-                // 200 -success code , content type in header text/html
-                response.writeHead(200,{"content-type":"text/html"});
-                if(result == undefined){
-                    records.push(input);    // added user and pass in loginDetails
-                    response.write("Account Created successfully!");     
-                    response.write(nodeTaskManagerPage);
-                    } else {
-                        response.write("Task ID must be unique!");     
-                        response.write(nodeTaskManagerPage); 
+            });
+            console.log(input.task_id);
+        } else if (result != undefined && input.task_id != undefined) {
+            console.log("here");
+            let index = records.indexOf(result.task_id);
+            records.splice(index, 1);
+            fs.writeFileSync("record.json", JSON.stringify(records), (error) => {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Data appended to file.")
                 }
-        } else {
-            response.write(indexPage);  
+            });
         }
+
+        fs.readFile("record.json", (error, data) => {
+            if (!error) {
+                if (data != "") {
+                let empString = data.toString();
+                let empJSON = JSON.parse(empString);
+                table = `<br>
+                <table border="1">
+                    <tr>
+                        <th>Employee ID</th>
+                        <th>Task ID</th>
+                        <th>Task</th>
+                        <th>Deadline</th>
+                    </tr>`;
+                empJSON.forEach(item => {
+                    table += `
+                        <tr>
+                            <td>${item.employee_id}</td>
+                            <td>${item.task_id}</td>
+                            <td>${item.task}</td>
+                            <td>${item.deadline}</td>
+                        </tr>
+                        `
+                });
+                console.log(empJSON);
+            }
+        }
+        response.end(indexPage + table + endOfIndexPage);
+        })
     }
-    
-    response.end();
 
 })
-
-function createRecord() {
-    let record = {
-        employeeID: id
-    };
-    records.push(record);
-    console.log("Pushed records.");
-}
-
 
 server.listen(9090,()=>console.log("Server running on port number 9090"))
